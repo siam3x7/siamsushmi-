5c335fafe600c5427de4bfaa60efbe5fce9627cc
siam3x7 <149674056+siam3x7@users.noreply.github.com>
2024-02-14 11:26:01 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function showFlowers() {
+    document.getElementById('flowersPopup').style.display = 'block';
+}
+
+function showMoreFlowers() {
+    document.getElementById('flowersPopup').style.display = 'block';
+}
+
+function closePopup() {
+    document.getElementById('flowersPopup').style.display = 'none';
+}
